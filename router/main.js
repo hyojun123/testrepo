48bb9fff5a5ee039a9046cd7c8ec1e46404487ce
CHOIHYOJUN <chlgkwk123@nate.com>
2019-06-10 11:12:19 +0900

    .

--- router/main.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(app) {
-    app.get('/', function(req, res) {
-        res.render('index/index.html');
-    });
-    app.get('/boards', function(req, res) {
-        res.render('board/main.html');
-    });
-}
